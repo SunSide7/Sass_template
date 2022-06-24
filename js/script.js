1a59704ad854df50fc43006c7bac55f88ca97ade
Ascended <Ascended@mail.com>
2022-06-24 13:28:05 +0200

    Stringify created

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// C:\Users\User\OneDrive\Рабочий стол\prepare path project\js\script.js
+
+$('.wrapper input').change(function() {
+    console.log('CHANGE');
+    console.log('EVENT TARGET VALUE:', event.target.value);
+
+    const result = []
+    const resultStringArray = event.target.value.split('\\');
+    
+    resultStringArray.forEach((stringItem, index) => {
+        
+        if (index !== resultStringArray.length - 1) {
+            console.log(stringItem);
+
+            result.push(stringItem);
+
+        }
+        
+    })
+
+    resultStringified_1 = result.join('\\');
+    resultStringified_2 = result.join('/');
+
+    const $template = `
+    <div class="result-string">${resultStringified_1}</div>
+    <br>
+    <br>
+    <div class="result-string">${resultStringified_2}</div>
+    `
+    
+    $('.result').append($template)
+})
