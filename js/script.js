3f60bfaaf13732c1d05abd2438838c02c9550d67
SunSide7 <DieMon91@list.ru>
2022-12-05 18:33:03 +0200

    CSS Constructor -> pt15 Create

--- js/script.js
+++ js/script.js
@@ -23,24 +23,50 @@ $('.wrapper input').change(function() {
     resultStringified_1 = result.join('\\');
     resultStringified_2 = result.join('/');
 
-    const $template = `
-    <div class="result-string copyToClipBoardBtn">/${resultStringified_1}</div>
-    <br>
-    <br>
-    <div class="result-string copyToClipBoardBtn">/${resultStringified_2}</div>
-    `
+    // const $template = `
+    // <div class="result-string copyToClipBoardBtn">/${resultStringified_1}</div>
+    // <br>
+    // <br>
+    // <div class="result-string copyToClipBoardBtn">/${resultStringified_2}</div>
+    // `;
+
+    const $template = `<div class="result-string copyToClipBoardBtn">/${resultStringified_2}</div>`;
+
+    
     
     $('.result').append($template)
 
     $('.copyToClipBoardBtn').each(function(index) {
 
-        if (index === 0) keyCode = 'Digit1';
-        if (index === 1) keyCode = 'Digit4';
+        // if (index === 0) keyCode = 'Digit1';
+        // if (index === 1) keyCode = 'Digit4';
+
+        if (index === 0) keyCode = 'Digit4';
+
+        
         
         $(this).copyUrlOnClick(false, 'Ссылка скопирована!', keyCode);
     })
 
+    window.navigator.clipboard.writeText(`/${resultStringified_2}`);
+
     event.target.value = '';
 
     $(this)[0].blur()
+})
+
+
+$(document).ready(function() {
+    setTimeout(() => {
+        $('.wrapper input').focus();
+    }, 10);
+
+    document.onkeyup = function (e) {
+        if (e.which == 82) {
+            // if (e.ctrlKey && e.shiftKey && e.which == 70) {
+            setTimeout(function () {
+                location.reload();
+            }, 10);
+        }
+    };
 })
